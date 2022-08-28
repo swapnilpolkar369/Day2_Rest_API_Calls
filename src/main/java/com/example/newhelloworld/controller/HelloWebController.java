b28a62ebc3561b64a0ec5b2c9d9414984921a02d
Swapnil <Swapnilpolkar@gmail.com>
2022-08-29 03:14:18 +0530

    Rest_Call_API

---
+++ src/main/java/com/example/newhelloworld/controller/HelloWebController.java
@@ -0,0 +1,35 @@
+package com.example.newhelloworld.controller;
+
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloWebController {
+
+    @GetMapping(value = {"","/","/home"})
+    public String Hello(){
+        return "Hello From Bridgelabz";
+    }
+    @GetMapping(value = {"/query"})
+    public String sayHello(@RequestParam String name) {
+        return " Hello " + name + "!";
+    }
+
+    @GetMapping("/param/{name}")
+    public String sayHelloParam(@PathVariable String name) {
+        return " Hello " + name + "!";
+    }
+
+    @PostMapping("/post")
+    public String sayHello(@RequestBody User user) {
+        return "Hello" + user.getFirstName() + "" + user.getLastName() + "!";
+    }
+
+    @PutMapping("/put/{firstName}")
+    public String sayHello(@PathVariable String firstName, @RequestParam(value = "lastName") String lastName) {
+        return "Hello" + firstName + "" + lastName + "!";
+    }
+}
+
